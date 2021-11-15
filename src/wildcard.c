eb81a63c51e9554279b8d422afe247fa68daf2de
Axel Coezard <acoezard@c2r5p8.42nice.fr>
2021-11-15 17:34:41 +0100

    deplacements de tests dans tests/

--- src/wildcard.c
+++ src/wildcard.c
@@ -6,21 +6,24 @@
 /*   By: acoezard <acoezard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 16:01:16 by acoezard          #+#    #+#             */
-/*   Updated: 2021/11/15 16:30:49 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/11/15 17:31:24 by acoezard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	wildcard_replace(t_token *tokens)
+static void	wildcard_replace(t_token *token)
 {
-	printf("%s\n", tokens->value);
+	token->expendable = 0;
+	printf("%s\n", token->value);
+	// TODO: #10 remplacer les * par leur valeur
 }
 
 void	wildcard_expand(t_token *tokens)
 {
 	while (tokens->value)
 	{
+		// TODO: #9 Verifier si il y a des ''
 		if (ft_strchr(tokens->value, '*'))
 			wildcard_replace(tokens);
 		tokens++;
