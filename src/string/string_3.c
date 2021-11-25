f2559db3e6a557eeb4cecaba26c993383cf76072
Mathias Berger--fruhinsholz <mberger-@c2r4p6.42nice.fr>
2021-11-25 11:29:02 +0100

    norm

--- src/string/string_3.c
+++ src/string/string_3.c
@@ -6,7 +6,7 @@
 /*   By: acoezard <acoezard@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:45:16 by acoezard          #+#    #+#             */
-/*   Updated: 2021/11/25 11:26:32 by acoezard         ###   ########.fr       */
+/*   Updated: 2021/11/25 11:28:26 by mberger-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,26 @@ int	ft_isspace(int c)
 
 int	is_valid_long(const char *nptr, long *v)
 {
-	int    neg;
+	int	neg;
 
-    *v = 0;
-    while ((*nptr >= '\t' && *nptr <= '\r') || *nptr == ' ')
-        nptr++;
-    neg = *nptr == '-';
-    if (*nptr == '-' || *nptr == '+')
-        nptr++;
-    if (!*nptr)
-        return (0);
-    while (*nptr >= '0' && *nptr <= '9')
-        if (*v * 10 - *nptr + '0' > *v)
-            return (0);
-    else
-        *v = *v * 10 - *nptr++ + '0';
-    if (!neg && *v < *v - 1)
-        return (0);
-    if (!neg)
-        *v = -*v;
-    if (*nptr)
-        return (0);
-    return (1);
+	*v = 0;
+	while ((*nptr >= '\t' && *nptr <= '\r') || *nptr == ' ')
+		nptr++;
+	neg = *nptr == '-';
+	if (*nptr == '-' || *nptr == '+')
+		nptr++;
+	if (!*nptr)
+		return (0);
+	while (*nptr >= '0' && *nptr <= '9')
+		if (*v * 10 - *nptr + '0' > *v)
+			return (0);
+	else
+		*v = *v * 10 - *nptr++ + '0';
+	if (!neg && *v < *v - 1)
+		return (0);
+	if (!neg)
+		*v = -*v;
+	if (*nptr)
+		return (0);
+	return (1);
 }
